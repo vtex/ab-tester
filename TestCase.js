c2dd6dab8ab3e982f6f1cccc44f1f6da02c586ee
Thiago Tergolino <thiago.tergolino@vtex.com.br>
2019-02-11 00:36:04 -0200

    Create functions for a preliminary test

--- TestCase.js
+++ TestCase.js
@@ -1,7 +1,19 @@
 var test = require('./test-evaluation');
 
+require('util');
+
 function ABtest()
 {
-    test.Evaluate(account, beginning, 'master', 'abtesting')
+    var winner = ' '
+    if(test.Evaluate(account, beginning, 'master', 'abtesting'))
+    {
+        winner = test.Evaluate(account, beginning, 'master', 'abtesting')
+    }
+    else
+    {
+        winner = 'none'
+    }
+    
+    return util.format('Winner: %s ; %s', winner, test.temporaryResult(account, beginning, 'master', 'abtesting'))
 }
 exports.ABtest = ABtest;
--- test-evaluation.js
+++ test-evaluation.js
@@ -1,10 +1,9 @@
+import { headerDefault, withAuthToken } from './headers'
 const axios = require('axios');
 
 var decisionRule = require('./decision-rule');
-import { headerDefault, withAuthToken } from './headers'
 
 require('util');
-
 
 function Evaluate(account, ABTestBeginning, workspaceA, workspaceB)
 {
@@ -54,3 +53,26 @@ function getDataStoreDash(endPoint)
       })
 }
 exports.getDataStoreDash = getDataStoreDash;
+
+function temporaryResult(account, ABTestBeginning, workspaceA, workspaceB)
+{
+    var endPointBounceSessions = util.format('http://api.vtex.com/api/storedash/%s/metrics/bouncesessions?from=%s&to=now&operation=sum&aggregateBy=workspace', account, ABTestBeginning),
+        endPointNoBounceSessions = util.format('http://api.vtex.com/api/storedash/%s/metrics/nobouncesessions?from=%s&to=now&operation=sum&aggregateBy=workspace', account, ABTestBeginning),
+        endPointOrderPlacedSessions = util.format('http://api.vtex.com/api/storedash/%s/metrics/orderplacedsessions?from=%s&to=now&operation=sum&aggregateBy=workspace', account, ABTestBeginning)
+
+    var ordersA = Ammount(endPointOrderPlacedSessions, workspaceA),
+        ordersB = Ammount(endPointOrderPlacedSessions, workspaceB),
+        bounceSessionsA = Ammount(endPointBounceSessions, workspaceA),
+        bounceSessionsB = Ammount(endPointBounceSessions, workspaceB),
+        noBounceSessionsA = Ammount(endPointNoBounceSessions, workspaceA),
+        noBounceSessionsB = Ammount(endPointNoBounceSessions, workspaceB)
+
+        var notConvertedA = bounceSessionsA + noBounceSessionsA,
+            notConvertedB = bounceSessionsB + noBounceSessionsB
+
+        var lossA = LossFunction(ordersA, notConvertedA, ordersB, notConvertedB),
+            lossB = LossFunction(ordersB, notConvertedB, ordersA, notConvertedA)
+
+        return util.format('Expected Loss Choosing A: %f ; Expected Loss Choosing B: %f', lossA, lossB)
+}
+exports.temporaryResult = temporaryResult;
