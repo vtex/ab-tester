091dddfd7766354ccf151089025371c450e26e2c
Thiago Tergolino <thiago.tergolino@vtex.com.br>
2019-02-18 00:53:43 -0300

    Creating method to save and read from vbase the information with the beginning of A/B Test

---
+++ node/vbase.ts
@@ -0,0 +1,50 @@
+import { VBase } from '@vtex/api'
+import { IOContext } from 'colossus'
+import { Readable } from 'stream'
+
+const bucket: string = 'master'
+const userAgent: string = 'VTEX AB-Tester'
+
+const toStream = (arg: any) => {
+  const readable = new Readable()
+  readable.push(JSON.stringify(arg))
+  readable.push(null)
+  return readable
+}
+
+export default function VBaseClient({ account, workspace, region, authToken }: IOContext) {
+    const client = new VBase({ account, workspace, region, authToken, userAgent })
+    const fileName = 'ab-test-data.txt'
+
+    return {
+        loadBeginning: async () => {
+          try {
+            const { data } = await client.getFile(bucket, fileName)
+            return data
+          } catch (err) {
+            if(err.status === 404) {
+              return []
+            }
+            throw err
+          }
+        },
+
+        finishABTest: async () => {
+          try {
+            return await client.saveFile(bucket, fileName, toStream({}), false)
+          } catch (err) {
+            throw err
+          }
+        },
+
+        saveAsBeginning: async () => {
+          try {
+            const currentTime = new Date()
+            const stream = toStream({'account': account, 'beginning': currentTime})
+            return await client.saveFile(bucket, fileName, stream, false)
+          } catch (err) {
+            throw err
+          }
+        },
+    }
+}
