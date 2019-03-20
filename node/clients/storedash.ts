1564b2445210d3a260db12fe845ef8ed1011359d
Thiago <thiago.tergolino@vtex.com>
2019-03-20 10:33:56 -0300

    Changing metric imported from storedash

--- node/clients/storedash.ts
+++ node/clients/storedash.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseURL = 'http://api.vtex.com/api/storedash/'
-const metricsStoredashPath = '/metrics/storedash/sessioncube'
+const metricStoredashPath = '/metrics/storedash/navigationcube'
 
 export async function getDataFromStoreDash(endPoint: string, ctx: ColossusContext): Promise<JSON[]> {
     return new Promise<JSON[]>((resolve, _reject) => {
@@ -22,32 +22,27 @@ export async function getDataFromStoreDash(endPoint: string, ctx: ColossusContex
 }
 
 export async function GetWorkspaceData(endPoint: string, workspace: string, ctx: ColossusContext) {
-    var metrics = await getDataFromStoreDash(endPoint, ctx)
+    const metrics = await getDataFromStoreDash(endPoint, ctx)
     var total = 0,
-        noOrders = 0
+        orderSessions = 0
     for (var metric of metrics) {
         if (metric["workspace"] == workspace) {
-            if (metric["data.orderPlaced"] == 'false') {
-                noOrders += metric["count"]
-                total += metric["count"]
-            }
-            else {
-                total += metric["count"]
-            }
+            orderSessions += metric["data.sessionsOrdered"]
+            total += metric["data.sessions"]
         }
     }
-    return WorkspaceData(workspace, total, noOrders)
+    return WorkspaceData(workspace, total, orderSessions)
 }
 
-export const WorkspaceData = (Workspace: string, TotalSessions: number, NoOrderSessions: number): WorkspaceData => ({
+export const WorkspaceData = (Workspace: string, TotalSessions: number, OrderSessions: number): WorkspaceData => ({
     Workspace: Workspace,
     Sessions: TotalSessions,
-    OrderSessions: (TotalSessions - NoOrderSessions),
-    NoOrderSessions: NoOrderSessions
+    OrderSessions: OrderSessions,
+    NoOrderSessions: (TotalSessions - OrderSessions)
 })
 
 export const AggregationQuery = (from: string): string => (
-    '?from=' + from + '&to=now&operation=sum&fields=count&aggregateBy=workspace,data.orderPlaced')
+    '?from=' + from + '&to=now&operation=sum&fields=data.sessions,data.sessionsOrdered&aggregateBy=workspace')
 
 export const StoreDashRequestURL = (account: string, ABTestBeginning: string): string => (
-    baseURL + account + metricsStoredashPath + AggregationQuery(ABTestBeginning))
+    baseURL + account + metricStoredashPath + AggregationQuery(ABTestBeginning))
