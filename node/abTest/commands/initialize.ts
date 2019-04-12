4df39e867b8331a5bac188651154f94ec3a01334
Thiago <thiago.tergolino@vtex.com>
2019-04-12 13:25:31 -0300

    Deleting unnecessary code

--- node/abTest/commands/initialize.ts
+++ node/abTest/commands/initialize.ts
@@ -23,18 +23,3 @@ export async function InitializeAbTestForWorkspace(ctx: ColossusContext): Promis
         throw new Error(err)
     }
 }
-
-export async function getWithRetriesHelper(retries: number, ctx: ColossusContext) {
-    let error = null
-    while (retries--) {
-        try {
-            return await InitializeAbTestForWorkspace(ctx)
-        } catch (err) {
-            err.response ?
-                console.error(`Error ${err.response.status} on initialize A/B Test, ${retries} retries left`) :
-                console.error(`Error on initialize A/B Test, ${retries} retries left`)
-            error = err
-        }
-    }
-    throw error
-}
