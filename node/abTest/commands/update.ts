929125cf8f14578a84810a9676e8736ee351acf3
caroldesenne <carolina.garcia@vtex.com.br>
2020-03-25 17:57:15 -0300

    Remove duplicated call to storage.getTestData
    And throw same exception that we received, instead of creating a new instance

--- node/abTest/commands/update.ts
+++ node/abTest/commands/update.ts
@@ -7,8 +7,8 @@ export async function UpdateStatusOnEvent(ctx: Context): Promise<void> {
   try {
     const workspacesMetadata = await abTestRouter.getWorkspaces(account)
     const testingWorkspaces = new TestingWorkspaces(workspacesMetadata)
+    const testData = await storage.getTestData(ctx)
     if (testingWorkspaces.Length() > 0) {
-      const testData = await storage.getTestData(ctx)
       let beginning = testData.dateOfBeginning
       let hours = testData.initialStageTime
       let proportion = testData.initialProportion
@@ -17,13 +17,13 @@ export async function UpdateStatusOnEvent(ctx: Context): Promise<void> {
         hours = 0
         proportion = 100
       }
-      const testType = (await storage.getTestData(ctx)).testType
+      const testType = (testData).testType
       const workspacesData: WorkspaceData[] = (testType === TestType.revenue) ? await GetGranularData(ctx) : await storedash.getWorkspacesData(beginning)
       await UpdateParameters(ctx, beginning, hours, proportion, workspacesData, testingWorkspaces, workspacesMetadata.id || 'noId', testType)
     }
   } catch (err) {
     logger.error({ status: err.status, message: err.message })
-    throw new Error(err)
+    throw err
   }
 }
 
