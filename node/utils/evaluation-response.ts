cf0d5f3b54b5e55ae2cc5967aba4ef938104c44f
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-09-17 17:21:12 -0300

    create DefaultEvaluationResponseRevenue

--- node/utils/evaluation-response.ts
+++ node/utils/evaluation-response.ts
@@ -21,6 +21,37 @@ export const DefaultEvaluationResponseConversion = (abTestBeginning: string, wor
     WorkspaceBSessionsLast24Hours: 0,
 })
 
+export const DefaultEvaluationResponseRevenue = (abTestBeginning: string, workspaceAName: string, workspaceBName: string): TestResult => ({
+    ABTestBeginning: abTestBeginning,
+    ConversionA: 0,
+    ConversionALast24Hours: 0,
+    ConversionB: 0,
+    ConversionBLast24Hours: 0,
+    ExpectedLossChoosingA: 0,
+    ExpectedLossChoosingB: 0,
+    OrdersValueA: 0,
+    OrdersValueALast24Hours: 0,
+    OrdersValueB: 0,
+    OrdersValueBLast24Hours: 0,
+    PValue: 0,
+    ProbabilityAlternativeBeatMaster: 0,
+    Winner: 'Not yet decided',
+    WorkspaceA: workspaceAName,
+    WorkspaceASessions: 0,
+    WorkspaceASessionsLast24Hours: 0,
+    WorkspaceB: workspaceBName,
+    WorkspaceBSessions: 0,
+    WorkspaceBSessionsLast24Hours: 0,
+    WinnerRevenue: 'Not yet decided',
+    PValueRevenue: 0,
+    ProbabilityAlternativeBeatsMasterRevenue: 0,
+    UValue: 0,
+    EffectSizeWorkspaceA: 0,
+    EffectSizeWorkspaceB: 0,
+    OrdersMedianWorkspaceA: 0,
+    OrdersMedianWorkspaceB: 0,
+})
+
 export const EvaluationResponse = (abTestBeginning: string, workspaceAData: WorkspaceCompleteData, workspaceBData: WorkspaceCompleteData, winner: string, lossA: number, lossB: number, probabilityOneBeatsTwo: number, pValue: number): TestResult => ({
     ABTestBeginning: abTestBeginning,
     ConversionA: workspaceAData.SinceBeginning.Conversion,
