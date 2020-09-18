976949a0c23c62370caa84cde37666bef1b7444f
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-09-18 16:22:37 -0300

    get test type from request

--- node/utils/BodyParser/getRequestParams.ts
+++ node/utils/BodyParser/getRequestParams.ts
@@ -2,11 +2,13 @@ import readBody from './readRequestBody'
 
 const expectedFields = ['InitializingWorkspace', 'Hours', 'Proportion']
 
-export default async (ctx: Context) => {
+export default async (ctx: Context) => {   
     const bodyContent = await readBody(ctx)
     checkForExpectedFields(bodyContent)
-    const { InitializingWorkspace, Hours, Proportion } = bodyContent
-    return { InitializingWorkspace, Hours, Proportion }
+    const { InitializingWorkspace, Hours, Proportion, Type } = bodyContent  
+    checkTestType(Type)
+
+    return { InitializingWorkspace, Hours, Proportion, Type }
 }
 
 const checkForExpectedFields = (object: object) => {
@@ -15,5 +17,11 @@ const checkForExpectedFields = (object: object) => {
         if (!(field in object)) {
             throw new Error(`Error getting request's parameters: make sure to set the ${field} field`)
         }
+    }
+}
+
+const checkTestType = (Type: string) => {
+    if (Type && Type !== 'conversion' && Type !== 'revenue') {
+        throw new Error(`Error setting test type: please make sure to spell it correctly (either 'conversion' or 'revenue')`)
     }
 }
