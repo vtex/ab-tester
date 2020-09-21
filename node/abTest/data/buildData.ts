929987457bda6a3c91a9f0acfcd605895225ed9d
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-09-21 11:02:00 -0300

    data / BuildCompleteData

--- node/abTest/data/buildData.ts
+++ node/abTest/data/buildData.ts
@@ -4,15 +4,20 @@ import { GetWorkspaceData } from '../../utils/workspace'
 import { Last24HoursData } from './recentData'
 
 export async function BuildCompleteData(account: string, abTestBeginning: string, workspacesData: WorkspaceData[], storedash: Storedash, router: Router): Promise<Map<string, WorkspaceCompleteData>> {
-    const workspacesNewData = await Last24HoursData(abTestBeginning, storedash)
-    const testingWorkspaces = await router.getWorkspaces(account)
-    const workspaceCompleteData = new Map()
-    for (const workspaceNewData of workspacesNewData) {
-        if(testingWorkspaces.Includes(workspaceNewData.Workspace)) {
-            workspaceCompleteData.set(workspaceNewData.Workspace, CompleteData(GetWorkspaceData(workspacesData, workspaceNewData.Workspace), workspaceNewData))
+    try {
+        const workspacesNewData = await Last24HoursData(abTestBeginning, storedash)
+        const testingWorkspaces = await router.getWorkspaces(account)
+        const workspaceCompleteData = new Map()
+        for (const workspaceNewData of workspacesNewData) {
+            if(testingWorkspaces.Includes(workspaceNewData.Workspace)) {
+                workspaceCompleteData.set(workspaceNewData.Workspace, CompleteData(GetWorkspaceData(workspacesData, workspaceNewData.Workspace), workspaceNewData))
+            }
         }
+        return workspaceCompleteData
+    } catch (err) {
+        err.message = `Error building test's data: ` + err.message
+        throw err
     }
-    return workspaceCompleteData
 }
 
 const CompleteData = (workspaceData: WorkspaceData, workspaceNewData: WorkspaceData): WorkspaceCompleteData => ({
