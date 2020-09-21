f55169bfa85187f8a1636d685f5cf5bba663e878
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-09-21 13:49:53 -0300

    finish / FinishAbTestForWorkspace

--- node/abTest/commands/finish.ts
+++ node/abTest/commands/finish.ts
@@ -8,52 +8,39 @@ import EndTest from '../endTest'
 export async function FinishAbTestForWorkspace(ctx: Context): Promise<void> {
   const { vtex: { account, route: { params: { finishingWorkspace } } }, clients: { abTestRouter, storage } } = ctx
   const workspaceName = firstOrDefault(finishingWorkspace)
-  let testingWorkspaces: TestingWorkspaces
-  try {
-    testingWorkspaces = await abTestRouter.getWorkspaces(account)
-  } catch (err) {
-    err.message = 'Error getting test metadata from router API'
-    ctx.vtex.logger.error({ status: ctx.status, message: err.message })
-    throw err
-  }
-  if (testingWorkspaces.Length() === 0) {
-    ctx.response.status = 404
-    throw new NotFoundError(`Test not initialized for this account`)
-  }
-
-  if (IsLastTestingWorkspace(testingWorkspaces)) {
-    return await EndTest(testingWorkspaces, ctx).catch(logErrorTest(ctx))
-  }
 
   try {
-    const testType = (await storage.getTestData(ctx)).testType
-    testingWorkspaces.Remove(workspaceName)
-    const testingParameters = createTestingParameters(testType, testingWorkspaces.ToArray())
-    await abTestRouter.setWorkspaces(account, {
-      id: testingWorkspaces.Id(),
-      workspaces: testingWorkspaces.ToArray(),
-    }).catch(logErrorTest(ctx))
-    const tsmap = new TSMap<string, ABTestParameters>([...testingParameters.Get()])
-    await abTestRouter.setParameters(account, {
-      Id: testingWorkspaces.Id(),
-      parameterPerWorkspace: tsmap,
-    }).catch(logErrorTest(ctx))
-    ctx.vtex.logger.info({ message: `A/B Test finished in ${account} for workspace ${workspaceName}`, account: `${account}`, workspace: `${workspaceName}`, method: 'TestFinished' })
-  } catch (err) {
-    if (err.status === 404) {
-      err.message = 'Workspace not found'
+
+    const testingWorkspaces = await abTestRouter.getWorkspaces(account)
+
+    if (testingWorkspaces.Length() === 0) {
+      ctx.response.status = 404
+      throw new NotFoundError(`Test not initialized for this account`)
     }
-    ctx.vtex.logger.error({ status: ctx.status, message: err.message })
+
+    if (IsLastTestingWorkspace(testingWorkspaces)) {
+      await EndTest(testingWorkspaces, ctx)
+    } 
+    else {
+      const testType = (await storage.getTestData(ctx)).testType
+      testingWorkspaces.Remove(workspaceName)
+      const testingParameters = createTestingParameters(testType, testingWorkspaces.ToArray())
+      await abTestRouter.setWorkspaces(account, {
+        id: testingWorkspaces.Id(),
+        workspaces: testingWorkspaces.ToArray(),
+      })
+      const tsmap = new TSMap<string, ABTestParameters>([...testingParameters.Get()])
+      await abTestRouter.setParameters(account, {
+        Id: testingWorkspaces.Id(),
+        parameterPerWorkspace: tsmap,
+      })
+      ctx.vtex.logger.info({ message: `A/B Test finished in ${account} for workspace ${workspaceName}`, account: `${account}`, workspace: `${workspaceName}`, method: 'TestFinished' })
+    }
+
+  } catch (err) {
+    err.message = 'Error finishing A/B test: ' + err.message
     throw err
   }
-}
-
-const logErrorTest = (ctx: Context) => (err: any) => {
-  if (err.status === 404) {
-    err.message = 'Test not found'
-  }
-  ctx.vtex.logger.error({ status: ctx.status, message: err.message })
-  throw err
 }
 
 const IsLastTestingWorkspace = (testingWorkspaces: TestingWorkspaces): boolean => {
