8ffc3e9a761837b20572337645343681374dfe99
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-09-21 10:35:25 -0300

    initialize / InitializeAbTest

--- node/abTest/commands/initialize.ts
+++ node/abTest/commands/initialize.ts
@@ -53,9 +53,9 @@ async function InitializeAbTest(workspaceName: string, hoursOfInitialStage: numb
         logger.info({message: `A/B Test initialized in ${account} for workspace ${workspaceName}`, account: `${account}`, workspace: `${workspaceName}`, proportion: `${proportionOfTraffic}`, type: `${testType}`, method: 'TestInitialized' })
     } catch (err) {
         if (err.status === 404) {
-            err.message = 'Workspace not found'
+            err.message = 'Workspace not found: ' + err.message
         }
-        logger.error({ status: ctx.status, message: err.message })
+        err.message = 'Error initializing A/B test: ' + err.message
         throw err
     }
 }
