48eb6af1be7329864c158088d444d27958b9cd14
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-09-29 15:09:54 -0300

    replace checks from initialize.ts

--- node/abTest/commands/initialize.ts
+++ node/abTest/commands/initialize.ts
@@ -2,8 +2,7 @@ import { TSMap } from 'typescript-map'
 import { createTestingParameters } from '../../typings/testingParameters'
 import { firstOrDefault } from '../../utils/firstOrDefault'
 import getRequestParams from '../../utils/Request/getRequestParams'
-import { concatErrorMessages } from '../../utils/errorHandling'
-import { WorkspaceMetadata } from '@vtex/api'
+import { checkTestType, checkIfNaN, CheckProportion, CheckWorkspace } from '../../utils/Request/Checks'
 
 export function InitializeAbTestForWorkspace(ctx: Context): Promise<void> {
     const workspace = ctx.vtex.route.params.initializingWorkspace
@@ -72,43 +71,4 @@ async function InitializeWorkspaces(ctx: Context, id: string, testingWorkspaces:
     }).catch((err) => {
         err.message = 'Error initializing workspaces: ' + err.message
     })
-}
-
-const checkTestType = (Type: string) => {
-    if (Type !== 'conversion' && Type !== 'revenue') {
-        throw new Error(`Error setting test type: please make sure to spell it correctly (either 'conversion' or 'revenue')`)
-    }
-}
-
-const checkIfNaN = (hours: string, proportion: string) => {
-    if (Number.isNaN(Number(hours))) {
-        throw new Error(`Error reading time parameter: make sure to insert a number`)
-    }
-    if (Number.isNaN(Number(proportion))) {
-        throw new Error(`Error reading proportion parameter: make sure to insert a number`)
-    }
-} 
-
-const CheckProportion = (proportion: number): number => {
-    return proportion >= 0 && proportion <= 10000 ? Math.round(proportion) : 10000
-}
-
-const CheckWorkspace = async (workspaceName: string, ctx: Context) => {    
-    if (workspaceName === 'master') {
-        throw new Error(`Bad workspace name: please select a workspace different from the master; the master workspace will be part of the test anyway`)
-    }
-    const account = ctx.vtex.account
-    let workspaces: WorkspaceMetadata[]
-    try {
-        workspaces = await ctx.clients.workspaces.list(account)
-    } catch (err) {
-        err.message = concatErrorMessages(`Error checking workspace name: Error fetching list of account's workspaces`, err.message)
-        throw err
-    }
-
-    for (const workspace of workspaces) {
-        if (workspace.production && workspaceName === workspace.name) return
-    }
-
-    throw new Error(`Bad workspace name: make sure to select one of your account's production workspaces`)
 }
--- node/utils/Request/Checks.ts
+++ node/utils/Request/Checks.ts
@@ -1,3 +1,6 @@
+import { concatErrorMessages } from '../../utils/errorHandling'
+import { WorkspaceMetadata } from '@vtex/api'
+
 const expectedFields = ['InitializingWorkspace', 'Hours', 'Proportion']
 
 export const checkForExpectedFields = (object: object) => {
@@ -13,4 +16,37 @@ export const checkTestType = (Type: string) => {
     if (Type !== 'conversion' && Type !== 'revenue') {
         throw new Error(`Error setting test type: please make sure to spell it correctly (either 'conversion' or 'revenue')`)
     }
+}
+
+export const checkIfNaN = (hours: string, proportion: string) => {
+    if (Number.isNaN(Number(hours))) {
+        throw new Error(`Error reading time parameter: make sure to insert a number`)
+    }
+    if (Number.isNaN(Number(proportion))) {
+        throw new Error(`Error reading proportion parameter: make sure to insert a number`)
+    }
+}
+
+export const CheckProportion = (proportion: number): number => {
+    return proportion >= 0 && proportion <= 10000 ? Math.round(proportion) : 10000
+}
+
+export const CheckWorkspace = async (workspaceName: string, ctx: Context) => {    
+    if (workspaceName === 'master') {
+        throw new Error(`Bad workspace name: please select a workspace different from the master; the master workspace will be part of the test anyway`)
+    }
+    const account = ctx.vtex.account
+    let workspaces: WorkspaceMetadata[]
+    try {
+        workspaces = await ctx.clients.workspaces.list(account)
+    } catch (err) {
+        err.message = concatErrorMessages(`Error checking workspace name: Error fetching list of account's workspaces`, err.message)
+        throw err
+    }
+
+    for (const workspace of workspaces) {
+        if (workspace.production && workspaceName === workspace.name) return
+    }
+
+    throw new Error(`Bad workspace name: make sure to select one of your account's production workspaces`)
 }
