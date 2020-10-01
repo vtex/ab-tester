a8aed35c34eb36fed5c265f4ec9d297735f4c521
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-10-01 10:53:18 -0300

    create FinishAbTest

--- node/abTest/commands/finish.ts
+++ node/abTest/commands/finish.ts
@@ -3,7 +3,23 @@ import { TSMap } from 'typescript-map'
 import { createTestingParameters } from '../../typings/testingParameters'
 import TestingWorkspaces from '../../typings/testingWorkspace'
 import { firstOrDefault } from '../../utils/firstOrDefault'
-import { EndTestForWorkspace } from '../endTest'
+import { EndTestForWorkspace, EndTest } from '../endTest'
+
+export async function FinishAbTest(ctx: Context): Promise<void> {
+  const { vtex: { account }, clients: {abTestRouter } } = ctx
+  try {
+    const testingWorkspaces = await abTestRouter.getWorkspaces(account)
+
+    if (testingWorkspaces.Length() === 0) {
+      ctx.status = 404
+      throw new NotFoundError(`Test not initialized for this account`)
+    }
+    await EndTest(testingWorkspaces, ctx)
+  } catch (err) {
+    err.message = 'Error finishing A/B test: ' + err.message
+    throw err
+  }
+}
 
 export async function FinishAbTestForWorkspace(ctx: Context): Promise<void> {
   const { vtex: { account, route: { params: { finishingWorkspace } } }, clients: { abTestRouter, storage } } = ctx
