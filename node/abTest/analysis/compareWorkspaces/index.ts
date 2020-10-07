7f525be68a1115be949d8e98e589bb35560c3734
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-10-07 15:29:25 -0300

    create WinnerOverAll

--- node/abTest/analysis/compareWorkspaces/index.ts
+++ node/abTest/analysis/compareWorkspaces/index.ts
@@ -1,5 +1,5 @@
-import { EvaluateConversion } from './conversion'
-import { EvaluateRevenue } from './revenue'
+import { EvaluateConversion, WinnerConversion } from './conversion'
+import { EvaluateRevenue, WinnerRevenue } from './revenue'
 
 export async function Evaluate(testType: TestType, abTestBeginning: string, workspaceAData: WorkspaceCompleteData, workspaceBData: WorkspaceCompleteData): Promise<TestResult> {
 
@@ -7,4 +7,11 @@ export async function Evaluate(testType: TestType, abTestBeginning: string, work
         return await EvaluateRevenue(abTestBeginning, workspaceAData, workspaceBData)
     }
     return await EvaluateConversion(abTestBeginning, workspaceAData, workspaceBData)
+}
+
+export function WinnerOverAll(testType: TestType, workspacesData: WorkspaceData[]): string {
+    if (testType === 'revenue') {
+        return WinnerRevenue(workspacesData)
+    }
+    return WinnerConversion(workspacesData)
 }
