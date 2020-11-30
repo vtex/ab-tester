26dfb394ee9693934d09af16fb8eecb3c9726405
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-11-30 11:14:54 -0300

    get approach from request

--- node/utils/Request/getRequestParams.ts
+++ node/utils/Request/getRequestParams.ts
@@ -4,7 +4,7 @@ import { checkForExpectedFields } from './Checks'
 export default async (ctx: Context) => {   
     const bodyContent = await readBody(ctx)
     checkForExpectedFields(bodyContent)
-    const { InitializingWorkspaces, Hours, Proportion, Type } = bodyContent  
+    const { InitializingWorkspaces, Hours, Proportion, Type, Approach } = bodyContent  
 
-    return { InitializingWorkspaces, Hours, Proportion, Type }
+    return { InitializingWorkspaces, Hours, Proportion, Type, Approach }
 }
