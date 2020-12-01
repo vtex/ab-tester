178a4db8dba31161ef3cb70836233e16e1a1c077
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-12-01 17:41:53 -0300

    adjust bayesConversion evaluation

--- node/abTest/analysis/compareWorkspaces/bayesianConversion.ts
+++ node/abTest/analysis/compareWorkspaces/bayesianConversion.ts
@@ -1,25 +1,25 @@
-import { DefaultEvaluationResponseConversion, EvaluationResponseConversion } from '../../../utils/evaluation-response'
+import { DefaultResponseBayesianConversion } from '../../../utils/evaluation-response/default'
+import { EvaluationResponseBayesianConversion } from '../../../utils/evaluation-response/evaluation'
 import { ChooseWinner, LossFunctionChoosingVariantOne, ProbabilityOfOneBeatsTwo } from '../../../utils/mathTools/decisionRule/bayesianConversion'
-import { BoundError, pValue } from '../../../utils/mathTools/forBetaDistribution/statistics/samplesRestrictions'
+import { BoundError } from '../../../utils/mathTools/forBetaDistribution/statistics/samplesRestrictions'
 import { WorkspaceToBetaDistribution } from '../../../utils/workspace'
 
-export async function EvaluateConversion(abTestBeginning: string, workspaceAData: WorkspaceCompleteData, workspaceBData: WorkspaceCompleteData): Promise<EvaluationResult> {
+export function Evaluate(abTestBeginning: string, workspaceAData: WorkspaceCompleteData, workspaceBData: WorkspaceCompleteData): BayesianEvaluationResultConversion {
     if (workspaceAData.SinceBeginning.Sessions === 0 || workspaceBData.SinceBeginning.Sessions === 0) {
-        return DefaultEvaluationResponseConversion(abTestBeginning, workspaceAData.SinceBeginning.Workspace, workspaceBData.SinceBeginning.Workspace)
+        return DefaultResponseBayesianConversion(abTestBeginning, workspaceAData.SinceBeginning.Workspace, workspaceBData.SinceBeginning.Workspace)
     }
 
     const betaDistributionA = WorkspaceToBetaDistribution(workspaceAData.SinceBeginning)
     const betaDistributionB = WorkspaceToBetaDistribution(workspaceBData.SinceBeginning)
     const lossA = LossFunctionChoosingVariantOne(betaDistributionA, betaDistributionB)
     const lossB = LossFunctionChoosingVariantOne(betaDistributionB, betaDistributionA)
-    const probabilityTwoBeatsOne = ProbabilityOfOneBeatsTwo(betaDistributionB.a, betaDistributionB.b, betaDistributionA.a, betaDistributionA.b)
-    const PValue = pValue(betaDistributionA, betaDistributionB)
+    const probabilityAbeatsB = ProbabilityOfOneBeatsTwo(betaDistributionA.a, betaDistributionA.b, betaDistributionB.a, betaDistributionB.b)
     const winner = ChooseWinner(workspaceAData.SinceBeginning, workspaceBData.SinceBeginning, BoundError) || 'Not yet decided'
 
-    return EvaluationResponseConversion(abTestBeginning, workspaceAData, workspaceBData, winner, lossA, lossB, probabilityTwoBeatsOne, PValue)
+    return EvaluationResponseBayesianConversion(abTestBeginning, workspaceAData, workspaceBData, winner, probabilityAbeatsB, lossA, lossB)
 }
 
-export function WinnerConversion(workspacesData: WorkspaceData[]): string {
+export function Winner(workspacesData: WorkspaceData[]): string {
     if (!workspacesData || workspacesData.length === 0) return 'master'
 
     let winner = workspacesData[0]
