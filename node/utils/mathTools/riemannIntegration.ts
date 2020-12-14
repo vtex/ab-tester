2b27ba45a0f2ed1f364017451c19fe0e32c538d4
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-12-14 12:00:43 -0300

    better integration function

--- node/utils/mathTools/riemannIntegration.ts
+++ node/utils/mathTools/riemannIntegration.ts
@@ -1,9 +1,11 @@
+import { Bounds } from './forNormalDistribution/densityEstimation'
+
 type Function = (x: number) => number
 const nSteps = 5e+4
  
 // The function below integrates f(x).g[0](y[0]).g[1](y[1])...g[len(g)-1](y[len(g)-1])
 // over the set of points (x, y) in (0,boundX) x (0,boundsY[0]) x ... x (0,boundsY[len(g)-1]) 
-// such that Y[i] < r[i]x for all i = 0,1,...len(g)-1
+// such that y[i] < r[i]x for all i = 0,1,...len(g)-1.
 
 export function probabilityUnderDiagonal(f: Function, g: Function[], r: number[], boundX: number, boundsY: number[]) {
     const n = g.length
@@ -55,29 +57,25 @@ export function differenceExpectationUnderDiagonal(f: Function, g: Function, rX:
     return I * boundX * actualBoundY / nSteps**2
 }
 
-/*
-*  At some point, we may want to restrict the intervals of integration's lower bounds, integrating
-*  over (fromX,toX) x (fromY[0],toY[0]) x ... x (fromY[len(g)-1],toY[len(g)-1]), 
-*  instead of performing the integration all the way from 0.
-*  This may be case, for example, if we realise that a store
-*  has a high convertion rate and a huge number of sessions.
-*/
+// The function below integrates f(x).g[0](y[0]).g[1](y[1])...g[len(g)-1](y[len(g)-1])
+// over the set of points (x, y) in boundX x boundsY[0] x ... x boundsY[len(g)-1]
+// that satisfy y[i] < x for all i = 0,1,...,len(g)-1.
 
-export function alternativeProbabilityUnderDiagonal(f: Function, g: Function[], r: number[], fromX: number, toX: number, fromY: number[], toY: number[]) {
+export function ProbabilityUnderDiagonal(f: Function, g: Function[], boundX: Bounds, boundsY: Bounds[]) {
     const n = g.length
 
-    const stepX = (toX-fromX) / (nSteps+1)
-    const to_Y = toY.map((to, idx) => Math.min(to, r[idx]*toX))
-    const stepY = to_Y.map((to, idx) => (to-fromY[idx])/(nSteps+1))
+    const stepX = (boundX.u-boundX.l) / (nSteps+1)
+    const upperBoundsY = boundsY.map(bound => Math.min(bound.u, boundX.u))
+    const stepY = boundsY.map((bound, idx) => (upperBoundsY[idx]-bound.l)/(nSteps+1))
 
     let partial = Array(n).fill(0)
     let I = 0
-    let pointX = fromX + stepX
-    let pointY = fromY.map((from, idx) => from+stepY[idx])
+    let pointX = boundX.l + stepX
+    let pointY = boundsY.map((bound, idx) => bound.l+stepY[idx])
 
     for (let i = 1; i <= nSteps; i++) {
         for (const j in pointY) {
-            while (pointY[j] <= Math.min(to_Y[j], r[j] * pointX)) { 
+            while (pointY[j] <= Math.min(upperBoundsY[j], pointX)) { 
                 partial[j] += g[j](pointY[j])
                 pointY[j] += stepY[j]
             }
@@ -85,7 +83,7 @@ export function alternativeProbabilityUnderDiagonal(f: Function, g: Function[], 
         I += f(pointX) * evaluateProduct(partial)
         pointX += stepX
     }
-    return I * (toX - fromX) * evaluateProduct(differences(to_Y, fromY)) / (nSteps**(n+1))
+    return I * (boundX.u - boundX.l) * evaluateProduct(interval_lengths(upperBoundsY, boundsY)) / (nSteps**(n+1))
 }
 
-const differences = (values1: number[], values2: number[]) => values1.map((value1, idx) => value1-values2[idx])
+const interval_lengths = (actualUpperBounds: number[], bounds: Bounds[]) => bounds.map((bound, idx) => actualUpperBounds[idx] - bound.l)
