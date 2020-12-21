b4eed2e4b5d3f61ab6d80302c0cca63750905ffd
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-12-21 16:06:37 -0300

    create Evaluate function

--- node/abTest/analysis/compareWorkspaces/frequentistRevenue.ts
+++ node/abTest/analysis/compareWorkspaces/frequentistRevenue.ts
@@ -1,7 +1,22 @@
 import { DefaultResponseFrequentistRevenue } from '../../../utils/evaluation-response/default'
+import { EvaluationResponseFrequentistRevenue } from '../../../utils/evaluation-response/evaluation'
+import { RevenueToNormalDistribution } from '../../../utils/workspace'
+import { PValue, PickWinner, UpLiftChoosingA } from '../../../utils/mathTools/decisionRule/frequentist'
 
 export function Evaluate(abTestBeginning: string, workspaceA: WorkspaceCompleteData, workspaceB: WorkspaceCompleteData): FrequentistEvaluationResultRevenue {
-    return DefaultResponseFrequentistRevenue(abTestBeginning, workspaceA.SinceBeginning.Workspace, workspaceB.SinceBeginning.Workspace)
+    if (workspaceA.SinceBeginning.Sessions === 0 || workspaceB.SinceBeginning.Sessions === 0) {
+        return DefaultResponseFrequentistRevenue(abTestBeginning, workspaceA.SinceBeginning.Workspace, workspaceB.SinceBeginning.Workspace)
+    }
+    
+    const parametersA = RevenueToNormalDistribution(workspaceA.SinceBeginning)
+    const parametersB = RevenueToNormalDistribution(workspaceB.SinceBeginning)
+
+    const pValue = PValue(parametersA, parametersB)
+    const winner = PickWinner(workspaceA.Last24Hours.Workspace, workspaceB.Last24Hours.Workspace, parametersA, parametersB, pValue)
+    const upLiftChoosingA = UpLiftChoosingA(parametersA, parametersB)
+    const upLiftChoosingB = UpLiftChoosingA(parametersB, parametersA)
+
+    return EvaluationResponseFrequentistRevenue(abTestBeginning, workspaceA, workspaceB, winner, pValue, upLiftChoosingA, upLiftChoosingB)
 }
 
 export function Winner(_workspacesData: WorkspaceData[]): string {
