893b32fced9753f64fec5bf317d2607a2205e4be
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-12-21 16:25:54 -0300

    create function for randomRestart

--- node/utils/randomExploration/relativePerformanceChange.ts
+++ node/utils/randomExploration/relativePerformanceChange.ts
@@ -1,6 +1,6 @@
 import { RandomBeta } from '../mathTools/forBetaDistribution/statistics/betaSampling'
 import { normalQuantile } from '../mathTools/forNormalDistribution/quantile'
-import { WorkspaceToBetaDistribution, RevenuePerConversion, RevenueToNormalDistribution } from '../workspace'
+import { WorkspaceToBetaDistribution, RevenuePerConversion, ConversionToNormalDistribution, RevenueToNormalDistribution } from '../workspace'
 
 export function relativeChange(approach: TestApproach, type: TestType, workspaceData: WorkspaceCompleteData): number {
     return relativeChangeFunctions[approach][type](workspaceData)
@@ -34,8 +34,11 @@ function relativeChangeBayesianRevenue(workspaceData: WorkspaceCompleteData): nu
     return (averageRevenueRecent*betaVariateRecent) / (averageRevenueOld*betaVariateOld)
 }
 
-function relativeChangeFrequentistConversion(_workspaceData: WorkspaceCompleteData): number {
-    return 1    // Provisional, until we implement the respective analysis
+function relativeChangeFrequentistConversion(workspaceData: WorkspaceCompleteData): number {
+    const normalVariateOld = normalQuantile(ConversionToNormalDistribution(workspaceData.SinceBeginning), Math.random())
+    const normalVariateRecent = normalQuantile(ConversionToNormalDistribution(workspaceData.Last24Hours), Math.random())
+
+    return normalVariateRecent / normalVariateOld
 }
 
 function relativeChangeFrequentistRevenue(workspaceData: WorkspaceCompleteData): number {
