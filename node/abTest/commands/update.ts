ebc61255cd6a58c13561be1153f7fca78dd75967
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-12-22 15:14:11 -0300

    add blank lines to improve readability

--- node/abTest/commands/update.ts
+++ node/abTest/commands/update.ts
@@ -3,14 +3,18 @@ import { UpdateProportions } from '../updateProportions'
 
 export async function UpdateStatusOnEvent(ctx: Context): Promise<void> {
   const { vtex: { account, logger }, clients: { abTestRouter, storedash, storage } } = ctx
+
   try {
     const testingWorkspaces = await abTestRouter.getWorkspaces(account)
+
     if (testingWorkspaces.Length() > 0) {
       const testData = await storage.getTestData(ctx)
+
       if (testData.isMAB || testData.isMAB === undefined) {
         let beginning = testData.dateOfBeginning
         let hours = testData.initialStageTime
         let initialMasterProportion = testData.initialProportion
+
         if (!(beginning && initialMasterProportion && hours !== undefined)) {
           beginning = new Date().toISOString().substr(0, 16)
           hours = 0
@@ -19,6 +23,7 @@ export async function UpdateStatusOnEvent(ctx: Context): Promise<void> {
         const testType = testData.testType
         const testApproach = testData.testApproach
         const workspacesData: WorkspaceData[] = (testApproach === 'frequentist' && testType === 'revenue') ? await GetGranularData(ctx) : await storedash.getWorkspacesData(beginning)
+
         await UpdateProportions(ctx, beginning, hours, initialMasterProportion, workspacesData, testingWorkspaces, testingWorkspaces.Id(), testType, testApproach)
       }
     }
