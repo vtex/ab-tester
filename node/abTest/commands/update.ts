8733408170ee8e897354c27230a01987906fb833
valentino-amadeus <valentino.sichinel@vtex.com.br>
2020-12-22 15:13:09 -0300

    check if isMAB before updating

--- node/abTest/commands/update.ts
+++ node/abTest/commands/update.ts
@@ -7,18 +7,20 @@ export async function UpdateStatusOnEvent(ctx: Context): Promise<void> {
     const testingWorkspaces = await abTestRouter.getWorkspaces(account)
     if (testingWorkspaces.Length() > 0) {
       const testData = await storage.getTestData(ctx)
-      let beginning = testData.dateOfBeginning
-      let hours = testData.initialStageTime
-      let initialMasterProportion = testData.initialProportion
-      if (!(beginning && initialMasterProportion && hours !== undefined)) {
-        beginning = new Date().toISOString().substr(0, 16)
-        hours = 0
-        initialMasterProportion = 5000
+      if (testData.isMAB || testData.isMAB === undefined) {
+        let beginning = testData.dateOfBeginning
+        let hours = testData.initialStageTime
+        let initialMasterProportion = testData.initialProportion
+        if (!(beginning && initialMasterProportion && hours !== undefined)) {
+          beginning = new Date().toISOString().substr(0, 16)
+          hours = 0
+          initialMasterProportion = 5000
+        }
+        const testType = testData.testType
+        const testApproach = testData.testApproach
+        const workspacesData: WorkspaceData[] = (testApproach === 'frequentist' && testType === 'revenue') ? await GetGranularData(ctx) : await storedash.getWorkspacesData(beginning)
+        await UpdateProportions(ctx, beginning, hours, initialMasterProportion, workspacesData, testingWorkspaces, testingWorkspaces.Id(), testType, testApproach)
       }
-      const testType = testData.testType
-      const testApproach = testData.testApproach
-      const workspacesData: WorkspaceData[] = (testApproach === 'frequentist' && testType === 'revenue') ? await GetGranularData(ctx) : await storedash.getWorkspacesData(beginning)
-      await UpdateProportions(ctx, beginning, hours, initialMasterProportion, workspacesData, testingWorkspaces, testingWorkspaces.Id(), testType, testApproach)
     }
   } catch (err) {
     err.message = 'Error on test update: ' + err.message
